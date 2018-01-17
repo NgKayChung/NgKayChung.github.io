fde71df951aaac921629e772203a91412a6aa4f7
Ng Kay Chung <ngkay_chung@hotmail.com>
2018-01-17 18:40:00 +0800

    Update validation.js

--- include/js/validation.js
+++ include/js/validation.js
@@ -10,6 +10,10 @@ function validateInputs()
     emailAddress = document.getElementById('emailAddress').value;
     phoneNumber = document.getElementById('phoneNumber').value;
     
+    if(fullname === "") {
+        alert('Name is required');
+    }
+    
     if(!fullname.match(/^[A-Za-z]+$/)) {
         alert('Name should consists of only alphabetic characters(A - Z)');
         return false;
