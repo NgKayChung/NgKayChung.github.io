9007e36fa302721e3d7c0ebe2270541cbc66648d
Ng Kay Chung <ngkay_chung@hotmail.com>
2018-01-23 13:09:50 +0800

    Update validation.js

--- include/js/validation.js
+++ include/js/validation.js
@@ -54,8 +54,8 @@ function validateInputs()
         return false;
     }
 
-    var emailRegex = /^[A-Za-z0-9\!\#\$\%\&\'\*\+\/\=\?\^\_\`\{\|\}\~\-]+(\.[A-Za-z0-9\!\#\$\%\&\'\*\+\/\=\?\^\_\`\{\|\}\~\-]+)?@[a-z0-9]+\.[a-z0-9]{2, 3}$/;
-     
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    //^[A-Za-z0-9\!\#\$\%\&\'\*\+\/\=\?\^\_\`\{\|\}\~\-]+(\.[A-Za-z0-9\!\#\$\%\&\'\*\+\/\=\?\^\_\`\{\|\}\~\-]+)?@[a-z0-9]+\.[a-z0-9]{2, 3}$/
     if(!emailAddress.match(emailRegex))
     { 
         alert("Invalid Email Address"); 
