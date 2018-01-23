3dbb1d788c48cfbfc7732f8c902788e586df8fee
Ng Kay Chung <ngkay_chung@hotmail.com>
2018-01-23 17:23:00 +0800

    Update validation_focus.js

--- include/js/validation_focus.js
+++ include/js/validation_focus.js
@@ -1,18 +1,18 @@
 function load()
 {
 	document.getElementbyId('fullname').addEventListener('onfocusout', function() {
-        this.style.border-color = "red";
+        this.style.borderColor = "red";
     });
 	
 	document.getElementbyId('icnumber').addEventListener('onfocusout', function() {
-        this.style.border-color = "red";
+        this.style.borderColor = "red";
     });
 	
 	document.getElementbyId('emailAddress').addEventListener('onfocusout', function() {
-        this.style.border-color = "red";
+        this.style.borderColor = "red";
     });
 	
 	document.getElementbyId('phoneNumber').addEventListener('onfocusout', function() {
-        this.style.border-color = "red";
+        this.style.borderColor = "red";
     });
 }
