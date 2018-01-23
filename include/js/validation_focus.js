2f7771f7b8f8f52efa7a4b0b8852242f4fb6b448
Ng Kay Chung <ngkay_chung@hotmail.com>
2018-01-23 17:46:20 +0800

    Update validation_focus.js

--- include/js/validation_focus.js
+++ include/js/validation_focus.js
@@ -1,7 +1,23 @@
 function load()
 {
 	document.getElementById('fullname').addEventListener('focusout', function() {
-        this.style.borderColor = "red";
+        var fullname = this.value;
+		
+		if(fullname === "")
+		{
+			this.parentNode.nextSibling.innerHTML = 'Name is required';
+			this.classList.add('err_input');
+			return;
+		}
+
+		if(!fullname.match(/^[A-Za-z \/\,\.\-]+$/))
+		{
+			this.parentNode.nextSibling.innerHTML = 'Invalid Name value');
+			this.classList.add('err_input');
+			return;
+		}
+		
+		this.classList.add('succ_input');
     });
 	
 	document.getElementById('icnumber').addEventListener('focusout', function() {
