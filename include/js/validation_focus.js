f8bb5c6f98ab9dc28d3c2b7919227f931e833a9a
Ng Kay Chung <ngkay_chung@hotmail.com>
2018-01-24 09:04:23 +0800

    Update validation_focus.js

--- include/js/validation_focus.js
+++ include/js/validation_focus.js
@@ -1,38 +1,127 @@
 function load()
 {
-	document.getElementById('fullname').addEventListener('focusin', function() {
-		this.classList.add("norm_input");
+	var textElems = document.querySelectorAll('input[type="text"]');
+	
+	textElems.forEach(function(elem) {
+		elem.addEventListener('focusin', function() {
+			var classes = this.classList;
+
+			if(classes.contains('err-input')) classes.remove('err-input');
+			else if(classes.contains('succ-input')) classes.remove('succ-input');
+
+			if(!classes.contains("norm_input")) classes.add('norm-input');
+		});
 	});
 	
 	document.getElementById('fullname').addEventListener('focusout', function() {
         var fullname = this.value;
+		var classes = this.classList;
+		//settimeout, loading/checking
 		
 		if(fullname === "")
 		{
 			document.getElementById("name-err").innerHTML = 'Name is required';
-			this.classList.add('err_input');
-			return;
+			classes.remove('norm-input');
+			classes.add('err_input');
+			return;//show cross
 		}
 
 		if(!fullname.match(/^[A-Za-z \/\,\.\-]+$/))
 		{
 			document.getElementById("name-err").innerHTML = 'Invalid Name value';
-			this.classList.add('err_input');
+			classes.remove('norm-input');
+			classes.add('err_input');
 			return;
 		}
-		
-		this.classList.add('succ_input');
+		//show tick
+		classes.remove('norm-input');
+		classes.add('succ_input');
     });
 	
 	document.getElementById('icnumber').addEventListener('focusout', function() {
-        this.style.borderColor = "red";
+        var ic = this.value;
+    	var classes = this.classList;
+		
+		if(ic === "")
+		{
+			document.getElementById("ic-err").innerHTML = 'IC Number is required';
+			classes.remove('norm-input');
+			classes.add('err_input');
+			return;
+		}
+
+		if(!ic.match(/\d{6}(?:[\-]|[\s])?\d{2}(?:[\-]|[\s])?\d{4}/))
+		{
+		  	document.getElementById("ic-err").innerHTML = 'Invalid IC Number';
+			classes.remove('norm-input');
+			classes.add('err_input');
+			return;
+		}
+
+		ic = ic.replace(/[^\d.]/g, "");
+
+		if(!validDOB(ic.substr(0, 6)) || !validState(ic.substr(6, 2)))
+		{
+		   	document.getElementById("ic-err").innerHTML = 'Invalid IC Number';
+			classes.remove('norm-input');
+			classes.add('err_input');
+			return;
+		}
+		
+		classes.remove('norm-input');
+		classes.add('succ_input');
     });
 	
 	document.getElementById('emailAddress').addEventListener('focusout', function() {
-        this.style.borderColor = "red";
+        var emailAddress = this.value;
+    	var classes = this.classList;
+		
+		if(emailAddress === "")
+		{
+			document.getElementById("email-err").innerHTML = 'Email Address is required';
+			classes.remove('norm-input');
+			classes.add('err_input');
+			return;
+		}
+
+		var emailRegex = /^[A-Za-z0-9\!\#\$\%\&\'\*\+\/\=\?\^\_\`\{\|\}\~\-]+(\.[A-Za-z0-9\!\#\$\%\&\'\*\+\/\=\?\^\_\`\{\|\}\~\-]+)?@[a-z0-9]+\.[a-z0-9]{2,3}$/;
+
+		if(!emailAddress.match(emailRegex))
+		{ 
+			document.getElementById("ic-err").innerHTML = 'Invalid Email Address';
+			classes.remove('norm-input');
+			classes.add('err_input');
+			return;
+		}
+		
+		classes.remove('norm-input');
+		classes.add('succ_input');
     });
 	
 	document.getElementById('phoneNumber').addEventListener('focusout', function() {
-        this.style.borderColor = "red";
+        var phoneNumber = this.value;
+		var classes = this.classList;
+		
+		if(phoneNumber === "")
+		{
+			document.getElementById("phone-err").innerHTML = 'Phone Number is required';
+			classes.remove('norm-input');
+			classes.add('err_input');
+			return;
+		}
+
+		var pnFormat = /^([0][1][0, 2-9]{1}([\s])?[\-]([\s])?\d{3}([\s])?\d{4})+$/;
+		var pnFormat011 = /^([0][1][1]([\s])?[\-]([\s])?\d{4}([\s])?\d{4})+$/;
+
+		if(!phoneNumber.match(pnFormat) && !phoneNumber.match(pnFormat011))
+		{
+			document.getElementById("phone-err").innerHTML = 'Invalid Phone Number';
+			classes.remove('norm-input');
+			classes.add('err_input');
+			return;
+		}
+		
+		classes.remove('norm-input');
+		classes.add('succ_input');
     });
 }
