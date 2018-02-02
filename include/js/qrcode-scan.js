3cffa37d60d17a649191e0920da09092df801bec
Ng Kay Chung <ngkay_chung@hotmail.com>
2018-02-02 11:22:45 +0800

    Update qrcode-scan.js

--- include/js/qrcode-scan.js
+++ include/js/qrcode-scan.js
@@ -13,7 +13,7 @@ var imghtml='<div id="qrfile">' +
 var imgsubhtml = '<img id = "output" src = ""/><div><button onclick = "submitImage()">Submit Image</button><img id = "decimg" src = ""/></div>';
 var fliphtml = '<div id = "flipbtn"><img src = "include/images/flip_cam_95px_76px.png" alt = "Change Camera"/></div>';
 var screenhtml = '<div id = "screenbtn"><img id = "size-img" src = "include/images/fullscreen_96px_95px.png"/></div>';
-var formhtml = '<div class = "contents">' +
+var formhtml = '<div class = "contents form-contents">' +
 '	<div class = "form-wrapper">' +
 		'<!--div class = "overlay-screen">' +
 		'<div class="lds-css ng-scope">' +
